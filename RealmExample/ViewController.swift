ee38b06db984f9896ceddae0eb951cdc2e3f1c34
Mahmoud Waked <mwaked@gmail.com>
2020-11-08 08:15:27 +0200

    Initial Commit

---
+++ RealmExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmExample
+//
+//  Created by Mahmoud Waked on 11/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
